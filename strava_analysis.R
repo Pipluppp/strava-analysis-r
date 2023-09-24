e7eb16af3ed6fa8b7b795cc4ebcb0bfc5c0c16d1
Pipluppp <duncanb013@gmail.com>
2023-09-24 15:46:26 +0800

    Update .qmd file with date and time fixes

--- strava_analysis.R
+++ strava_analysis.R
@@ -8,9 +8,13 @@
 #' 
 #' ## Data tidy
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 library(tidyverse)
 library(janitor)
+library(viridis)
+library(scales)
+library(ggridges)
+
 data <- read_csv("activities.csv")
 data <- data |> janitor::clean_names()
 data
@@ -18,55 +22,49 @@ data
 #' 
 #' Select needed columns and only take runs
 #' 
-## --------------------------------------------------------------------------------------------------------------
-
+## ----------------------------------------------------------------------------------------------------------------
 data_select <- data |> 
     select(activity_id:activity_type) |> 
     filter (activity_type == "Run")
 
 #' 
-#' Separate into `date` and `time` columns
+#' Convert to POSIXct and fixes 8-hour time difference from Strava export
 #' 
-## --------------------------------------------------------------------------------------------------------------
-data_split_date <- data_select |> 
-    separate(activity_date, c('date', 'time'), sep = ", (?=[^,]*$)")
-data_split_date
+## ----------------------------------------------------------------------------------------------------------------
+data_select$activity_date <- data_select$activity_date |>
+    lubridate::parse_date_time("mdyIMs p") |> # month, day, year, hour, minute, sec, pm/am
+    (`+`)(hours(8)) # adds hours(8), implicit function using ()()
 
 #' 
-#' Turn date into proper date formate, make a day of the week `wday` column
+#' Add `date` and `wday` columns
 #' 
-## --------------------------------------------------------------------------------------------------------------
-
-data_proper_date <- data_split_date |> 
-    # turn to system's date format
-    mutate(date = as.Date(date, format = "%b %d, %Y")) |> 
-    mutate(wday = weekdays(date))
+## ----------------------------------------------------------------------------------------------------------------
+data_date <- data_select |> 
+    mutate(
+        date = lubridate::as_date(activity_date), .after = activity_date,
+        wday = wday(date, label = TRUE)
+    )
 
 #' 
-#' Complicated magic to finally handle time, and add the 8 hours (can't make timezone functions work)
+#' Reuse `activity_date` column as new `time` column
 #' 
-## --------------------------------------------------------------------------------------------------------------
-# Play with proper_time before appending to dataframe
-proper_time <- data_proper_date$time
+## ----------------------------------------------------------------------------------------------------------------
+data_time <- data_date |> 
+    rename(time = activity_date) |> 
+    relocate(time, .after = date)
 
-proper_time <- proper_time |> 
-    strptime("%I:%M:%S %p",  tz = "UTC") |> 
+#' 
+#' Remove date component of `time` (turn data type to hms, then back to POSIXct)
+#' 
+## ----------------------------------------------------------------------------------------------------------------
+data_clean <- data_time
+
+data_clean$time <- hms::hms(
+    second(data_time$time),
+    minute(data_time$time),
+    hour(data_time$time)
+    ) |> 
     as.POSIXct()
-
-proper_time <- hms::hms(second(proper_time), minute(proper_time), hour(proper_time))
-
-# finally on datetime format that can be added to 
-proper_time <- proper_time |> 
-    as.POSIXct() + hours(8)
-
-
-
-#' 
-#' Append fixed time to dataframe
-#' 
-## --------------------------------------------------------------------------------------------------------------
-data_fixed_time <- data_proper_date
-data_fixed_time$time <- proper_time
 
 #' 
 #' ## Data visualizations
@@ -77,44 +75,41 @@ data_fixed_time$time <- proper_time
 #' 
 #' Chart runs by day of the week
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 #| label: bar-chart
 
-week <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 ggplot(
-    data_proper_date, aes(x = wday)
+    data_clean, aes(x = wday)
 ) + 
     geom_bar() +
     # labels
-    labs(x = "Day of the Week", y = "Count") +
-    # order day of the weeks properly
-    scale_x_discrete(limits = week)
+    labs(x = "Day of the Week", y = "Count")
 
 #' 
 #' ### Density plot
 #' 
 #' A density plot on the time of start of runs
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 #| label: density
 
-library(scales)
-ggplot(data_fixed_time) + 
+ggplot(data_clean) + 
     geom_density(
         aes(x = time, y = after_stat(scaled)),
         fill = "darkcyan", alpha = 0.8
     ) +
-    # fix x ticks
+    
+    # show only hours and mins for x ticks 
     scale_x_datetime(breaks = date_breaks("2 hours"), labels=date_format("%H:%M"))
 
 
 #' 
 #' ### Histogram
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 #| label: histogram
 
-ggplot(data_fixed_time) + 
+ggplot(data_clean) + 
     geom_histogram(aes(x = time), fill = "darkcyan", alpha = 0.8) +
     scale_x_datetime(
         breaks = date_breaks("2 hours"),  labels = date_format("%H:%M")
@@ -125,11 +120,10 @@ ggplot(data_fixed_time) +
 #' 
 #' Density plots like previous but superimposed across each day of the week
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 #| label: density-multi
 
-library(viridis)
-ggplot(data_fixed_time) + 
+ggplot(data_clean) + 
     geom_density(
         aes(x = time, group = wday, fill = wday, y = after_stat(scaled)), alpha = 0.5
     ) + 
@@ -137,8 +131,7 @@ ggplot(data_fixed_time) +
     labs(
         x = "Time", 
         group = "Day of the week", fill = "Day of the week",
-        ) +
-    # x ticks by 
+    ) +
     scale_x_datetime(breaks = date_breaks("2 hours"), labels = date_format("%H:%M")) + 
     # fancier colors from viridis
     scale_fill_viridis(discrete = TRUE) 
@@ -149,18 +142,15 @@ ggplot(data_fixed_time) +
 #' 
 #' Like multiple densities but separated by day, less messier
 #' 
-## --------------------------------------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------
 #| label: ridgeline
 
-library(ggridges)
-ggplot(data_fixed_time) +
+ggplot(data_clean) +
     geom_density_ridges(
         aes(x = time, y = wday, fill = wday), 
         alpha = 0.7, scale = 1, show.legend = FALSE
     ) +
     labs(x = "Time", y = "Day") +
-    # order the wday (day of the week)
-    scale_y_discrete(limits = week) + 
     # x-axis name and intervals
     scale_x_datetime(breaks = date_breaks("2 hours"),  labels = date_format("%H:%M")) + 
     # coloring
