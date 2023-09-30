943bb9fd2f24b6f19d148839733d1bf6f52e6fce
Pipluppp <duncanb013@gmail.com>
2023-10-01 02:36:34 +0800

    Dataframe for total daily mileage, and more visualizations

--- 03-exploratory-analysis.R
+++ 03-exploratory-analysis.R
@@ -25,26 +25,45 @@ ggplot(df_month_week_day) +
 
 # Box plot of distance by month
 ggplot(df_month_week_day) + 
-    geom_boxplot(aes(x = month, y = moving_time_m))
+    geom_boxplot(aes(x = month, y = distance_km))
 
 
 # Mileage for the day than distance of a run
 # since I tend to do multiple Strava run sessions (warm up, cooldown, etc.)
 
-# Get mileage per day, then plot
-df_month_week_day |> 
+# New dataframe for mileage per day
+df_daily_mileage <- df_month_week_day |> 
     group_by(date) |> 
     summarise(mileage = sum(distance_km)) |> 
+    
+    mutate(month = lubridate::month(date, label = TRUE)) |> 
+    mutate(week = lubridate::week(date))
+
+# Scatterplot using daily mileage
+df_daily_mileage |>  
     ggplot(aes(x = date, y = mileage)) +
     geom_point() + 
     geom_smooth(method = "lm")
 
-    
+# Column chart of mileage per month
+df_daily_mileage |> 
+    ggplot(aes(x = month, y = mileage)) +
+    geom_col()
 
+# Column chart of mileage per week
+df_daily_mileage |>  ggplot(aes(x = week, y = mileage)) +
+    geom_col()
 
+# Boxplot using daily mileage
+df_daily_mileage |> 
+    ggplot(aes(x = month, y = mileage)) +
+    geom_boxplot()
 
-
-
-
-
-
+# Barplot of number of days I ran each month
+df_daily_mileage |> 
+    ggplot(aes(x = month)) +
+    geom_bar() +
+    labs(
+        title = "Ran the most days during March",
+        x = "Month", y = "Number of days"
+    )
